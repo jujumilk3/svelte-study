db79f8632298d7faf52c2be0163eb5a161142794
jujumilk3 <jujumilk3@gmail.com>
2023-05-17 23:15:15 +0900

    add custom components

---
+++ super-simple-blog/src/lib/components/buttons/Button.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    export let buttonName = "Button";
+</script>
+
+<button type="button" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+    {buttonName}
+</button>
--- super-simple-blog/src/routes/test/+page.svelte
+++ super-simple-blog/src/routes/test/+page.svelte
@@ -1,3 +1,11 @@
+<script lang="ts">
+    import Button from "$lib/components/buttons/Button.svelte";
+    let name = "world";
+</script>
+
 <h1>
     test page
 </h1>
+<Button buttonName="thisisbuttonname"/>
+<br/><br/>
+<Button buttonName="{name}"/>
