a491bf4e7113ce3e14486b01d8654629f79025a7
jujumilk3 <jujumilk3@gmail.com>
2023-05-17 23:31:06 +0900

    custom button event forward

--- super-simple-blog/src/lib/components/buttons/Button.svelte
+++ super-simple-blog/src/lib/components/buttons/Button.svelte
@@ -2,6 +2,8 @@
     export let buttonName = "Button";
 </script>
 
-<button type="button" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+<!-- 여기 on:click을 갈겨줘야 컴포넌트에서 삽입을 했을 때 받을 수 있음. -->
+<button on:click
+        type="button" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
     {buttonName}
 </button>
--- super-simple-blog/src/routes/test/+page.svelte
+++ super-simple-blog/src/routes/test/+page.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
     import Button from "$lib/components/buttons/Button.svelte";
     let name = "world";
+    const handleClick = async (e) => {
+        alert("clicked");
+        console.log(e);
+    }
 </script>
 
 <h1>
     test page
 </h1>
-<Button buttonName="thisisbuttonname"/>
-<br/><br/>
-<Button buttonName="{name}"/>
+<div>
+    <button on:click={(e) => handleClick(e)}>Button created inner page</button>
+    <h2>Button tests by components</h2>
+    <Button buttonName="onclick test" on:click={(e) => handleClick(e)}/>
+    <br/><br/>
+    <Button buttonName="thisisbuttonname"/>
+    <br/><br/>
+    <Button buttonName="{name}"/>
+</div>
